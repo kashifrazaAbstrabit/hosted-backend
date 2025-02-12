8fb834acb79ce0e3481f418574da3479a30ce573
Kashif Raza <kashifr@abstrabit.com>
2025-02-12 22:26:38 +0530

    added some changes

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://kaleidoscopic-empanada-97d619.netlify.app",
+      "https://hosted-frontend-8d7h04xyq-kashifrazaabstrabits-projects.vercel.app"
     ], // Frontend URL
     credentials: true, // Allow cookies and credentials
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
